efe52dec162bb5897ff68d1fced8897c95b79600
Andreas Jakl <andreas.jakl@live.com>
2017-12-29 09:57:33 +0100

    Additional comments

--- public/language.js
+++ public/language.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+    // This method gets executed when the page DOM is ready
     console.log("Document ready");
+    // Ensure the error message is hidden when the page is loaded
     $('.errormsg').hide();
+    // Initialize the socket.io library
     var socket = io();
 
     $("#assessment_form").submit(function(e){
@@ -22,15 +25,20 @@ $(document).ready(function () {
     });
     
     socket.on('Temperature', function(msg){
+        // Temperature intent was recognized - update UI with new entity value
         $('.result_temperature').html(msg);
     });
     socket.on('PupillaryResponse', function(msg){
+        // PupillaryResponse intent was recognized - update UI with new entity value
         $('.result_pupillaryresponse').html(msg);
     });
     socket.on('Age', function(msg){
+        // Age intent was recognized - update UI with new entity value
         $('.result_age').html(msg);
     });
     socket.on('Error', function(msg){
+        // Error message was received
+        // Update UI with the error message and make sure the div is visible
         $('.errormsg').html(msg);
         $('.errormsg').show();
     });
