79d198215064f659be95759bfffadcdcde8847ba
Andreas Jakl <andreas.jakl@live.com>
2020-11-12 14:07:51 +0100

    Update to LUIS API v3. Replace deprecated request module with node-fetch. Update Bootstrap and jQuery.

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var request = require('request');
-var querystring = require('querystring');
+var io = require('socket.io')(http, {
+    perMessageDeflate: false
+});
+const fetch = require('node-fetch');
+var querystring = require('querystring');   // Part of Node.js
 
-const ENDPOINT = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
 const APPID = "";
 const APPKEY = "";
+const ENDPOINT = `https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/${APPID}/slots/production/predict`;
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-io.on('connection', function (socket) {
-    socket.on('assessment', function (msg) {
-        sendToLuis(msg, socket.id);
+// Note: if deployed on Azure, web sockets might fail if deployed to Linux free tier.
+// See: https://docs.microsoft.com/en-us/azure/app-service/faq-app-service-linux#web-sockets
+// https://github.com/MicrosoftDocs/azure-docs/issues/49245
+// socket.io still finds a way to connect, so you get an error message in the browser console,
+// but it still works.
+
+io.on('connection', async (socket) => {
+    socket.on('assessment', async (msg) => {
+        console.log("Got assessment: " + msg);
+        await sendToLuis(msg, socket.id);
     });
 });
 
-function sendToLuis(assessment, socketId) {
+async function sendToLuis(assessment, socketId) {
     // Check if we have all the data we need
     if (!ENDPOINT || !APPID || !APPKEY) {
         io.to(socketId).emit('Error', 'Missing configuration on the server.');
@@ -31,41 +41,55 @@ function sendToLuis(assessment, socketId) {
     // Send to LUIS
     var queryParams = {
         "subscription-key": APPKEY,
-        "verbose": true, // If true will return all intents instead of just the topscoring intent
-        "q": assessment
+        "verbose": true,    // We need verbose so that we can access the entity data in a generic way
+        "show-all-intents": false,
+        "log": true,        // Also log query to LUIS console
+        "query": assessment
     }
 
     var luisRequest =
-        ENDPOINT + APPID +
+        ENDPOINT +
         '?' + querystring.stringify(queryParams);
 
-    request(luisRequest,
-        function (err, response, body) {
-            if (err) {
-                console.log(err);
-                io.to(socketId).emit('Error', 'Error understanding your assessment: ' + err);
-            } else {
-                var data = JSON.parse(body);
-
-                // Check if the relevant properties exist
-                if (!data.hasOwnProperty('query') ||
-                    !data.topScoringIntent.hasOwnProperty('intent') ||
-                    !data.entities[0] ||
-                    !data.entities[0].hasOwnProperty('entity')) {
-                    console.log('Missing query, intent or entity: ');
-                    console.log(data);
-                    io.to(socketId).emit('Error', 'Sorry, I could not understand that. [No intent or entity identified]');
-                    return;
-                }
-
-                console.log(`Query: ${data.query}`);
-                console.log(`Top Intent: ${data.topScoringIntent.intent}`);
-                console.log(`Entity: ${data.entities[0].entity}`);
-
-                io.to(socketId).emit(data.topScoringIntent.intent, data.entities[0].entity);
-            }
+    try {
+        const response = await fetch(luisRequest);
+        if (!response.ok) {
+            console.log('Error with request: ' + response.statusText);
+            io.to(socketId).emit('Error', 'Sorry, I had problems with the request. ' + response.statusText);
         }
-    );
+        const data = await response.json();
+        console.log("Received data: " + JSON.stringify(data));
+
+        // // Check if the relevant properties exist
+        if (!data.prediction.entities.$instance ||
+            !Object.keys(data.prediction.entities.$instance) ||
+            !Object.keys(data.prediction.entities.$instance)[0] ||
+            !data.prediction.entities.$instance[Object.keys(data.prediction.entities.$instance)[0]][0].text) {
+            console.log('Missing query, intent or entity: ');
+            console.log(data);
+            io.to(socketId).emit('Error', 'Sorry, I could not understand that. [No intent or entity identified]');
+            return;
+        }
+
+        const instance = data.prediction.entities.$instance;
+        //console.log("Entity $instance: " + JSON.stringify(instance));
+        //console.log("Name of first element in $instance (not an array!): " + Object.keys(instance)[0]);
+        const firstInstance = Object.keys(instance)[0];
+        //console.log("Accessing first element in $instance: " + JSON.stringify(instance[firstInstance]));
+        //console.log("Text in first element in $instance: " + instance[firstInstance][0].text);
+        const firstInstanceText = instance[firstInstance][0].text;
+
+        console.log(`Query: ${data.query}`);
+        console.log(`Top Intent: ${data.prediction.topIntent}`);
+        console.log(`Entity text: ${firstInstanceText}`);
+
+        io.to(socketId).emit(data.prediction.topIntent, firstInstanceText);
+    } catch (error) {
+        console.log(error);
+        io.to(socketId).emit('Error', 'Error understanding your assessment: ' + error);
+    }
+
 }
+
 
 http.listen(PORT, () => console.log(`Vital Signs Checklist server listening on port ${PORT}!`));
